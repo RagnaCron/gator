d0e534b6f4b5efde29b79efa1d7ed2975b66b66b
Manuel Werder <manuelwerder@hotmail.com>
2026-02-24 14:58:20 +0100

    Setting up the config...

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path/filepath"
+)
+
 const configFileName = "config.json"
 
 type Config struct {
@@ -7,19 +14,74 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
-func (c *Config) SetUser(userName string) error {
+func (c Config) SetUser(userName string) error {
+	if userName == "" {
+		return errors.New("you need to set a user name")
+	}
+
+	c.CurrentUserName = userName
+
+	err := write(c)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func Read() Config {
+func Read() (Config, error) {
 	var config Config
-	return config
+
+	path, err := getConfigFilePath()
+	if err != nil {
+		return config, err
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, err
+	}
+
+	return config, nil
 }
 
 func getConfigFilePath() (string, error) {
-	return "", nil
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configPath := filepath.Join(home, ".config", "gogator")
+	err = os.MkdirAll(configPath, 0744)
+	if err != nil {
+		return "", err
+	}
+
+	filePath := filepath.Join(configPath, configFileName)
+
+	return filePath, nil
 }
 
 func write(cfg Config) error {
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+
+	path, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(path, data, 0640)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- main.go
+++ main.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	c "github.com/ragnacron/gogator/internal/config"
 )
 
 func main() {
-	config := c.Read()
-	config.SetUser("ragnacron")
-	config = c.Read()
+	config, err := c.Read()
+	logFatalln(err)
+
+	err = config.SetUser("ragnacron")
+	logFatalln(err)
+
+	config, err = c.Read()
+	logFatalln(err)
+
 	fmt.Println(config.DBUrl)
 	fmt.Println(config.CurrentUserName)
+}
+
+func logFatalln(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
