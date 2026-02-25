e3a35088b8fb084e872e7bad27db7f342614d8ce
Manuel Werder <manuelwerder@hotmail.com>
2026-02-25 01:31:16 +0100

    Refactored sql

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,9 +1,9 @@
 -- +goose Up 
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY,
-    created_at TIMESTAMP,
-    updated_at TIMESTAMP,
-    name TEXT UNIQUE
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    name TEXT NOT NULL UNIQUE
 );
 
 -- +goose Down
