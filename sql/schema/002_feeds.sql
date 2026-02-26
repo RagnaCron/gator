89b7cf7f820d276b55e4e755d3da24ec595ae0cc
Manuel Werder <manuelwerder@hotmail.com>
2026-02-26 01:05:15 +0100

    making url on feeds unique

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS feeds (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
     name TEXT NOT NULL UNIQUE,
-    url TEXT NOT NULL,
+    url TEXT NOT NULL UNIQUE,
     user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE
 );
 
