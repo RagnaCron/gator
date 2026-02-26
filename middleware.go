9756ca7af686abd791b98f613a1e39c2f6095ed1
Manuel Werder <manuelwerder@hotmail.com>
2026-02-26 16:02:38 +0100

    Added middleware for LoggindIn users

--- middleware.go
+++ middleware.go
@@ -8,7 +8,7 @@ import (
 )
 
 func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
-	f := func(s *state, cmd command) error {
+	return func(s *state, cmd command) error {
 		user, err := s.db.GetUser(context.Background(), s.config.CurrentUserName)
 		if err != nil {
 			return fmt.Errorf("couldn't find user: %w", err)
@@ -16,6 +16,4 @@ func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) 
 
 		return handler(s, cmd, user)
 	}
-
-	return f
 }
